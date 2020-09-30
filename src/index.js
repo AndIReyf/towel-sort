2940eeb411dd2edf57c1fd0351c6268f2c1322b8
AndIReyf <andrey.prudius@hotmail.com>
2020-09-30 12:32:33 +0200

    feat: add task towel sort solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+module.exports =
+    function towelSort(matrix) {
+        if (!matrix || matrix.length === 0) return []
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+        return matrix.reduce((arr, item, index) => {
+            if (index % 2 === 1) {
+                item.reverse()
+            }
+            return arr.concat(item)
+        }, [])
+    }
